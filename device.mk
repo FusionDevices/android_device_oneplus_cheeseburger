a09c60b42a59b16d75025682f07245ca8a61a334
siankatabg <siankata91@gmail.com>
2017-09-01 18:10:38 +0100

    Cheeseburger: Audio: All configs back to stock and update to 4.5.10
    Replacing all configs with those from stock 4.5.10, added few missing ones(with modifications from OnePlus) and reworked audio_platform_info.xml using https://github.com/OnePlusOSS/android_hardware_qcom_audio/commit/028d69d56306502358ed80c5f4703d8b10310044
    
    This fix the broken in-call earpiece/speaker volume control, all echo issues, WhatsApp low recording volume and maybe more!
    
    Change-Id: I8362fea3b550f281b90cf652a1cce115b5bf96a6
    Signed-off-by: siankatabg <siankata91@gmail.com>

--- device.mk
+++ device.mk
@@ -124,6 +124,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/aanc_tuning_mixer.txt:system/etc/aanc_tuning_mixer.txt \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
+    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
+    $(LOCAL_PATH)/audio/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     $(LOCAL_PATH)/audio/listen_platform_info.xml:system/etc/listen_platform_info.xml \
     $(LOCAL_PATH)/audio/mixer_paths_i2s.xml:system/etc/mixer_paths_i2s.xml \
     $(LOCAL_PATH)/audio/mixer_paths_qvr.xml:system/etc/mixer_paths_qvr.xml \
@@ -136,8 +138,6 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/a2dp_audio_policy_configuration.xml:/system/etc/a2dp_audio_policy_configuration.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/audio_policy_volumes.xml:/system/etc/audio_policy_volumes.xml \
-    $(TOPDIR)frameworks/av/services/audiopolicy/config/default_volume_tables.xml:/system/etc/default_volume_tables.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
